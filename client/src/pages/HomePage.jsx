88457fcd906b18edbe773ca6004357e0719bd83f
Jebidyha <watson20@me.com>
2023-06-18 09:12:22 -0400

    remove junk from homepage

--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -6,28 +6,8 @@ import RouteMap from "../routes";
 import Button from 'react-bootstrap/Button';
 
 const HomePage = () => {
-    return (
-        <>
-        <Button variant="primary">Primary</Button>{' '}
-        <Button variant="secondary">Secondary</Button>{' '}
-        <Button variant="success">Success</Button>{' '}
-        <Button variant="warning">Warning</Button>{' '}
-        <Button variant="danger">Danger</Button>{' '}
-        <Button variant="info">Info</Button>{' '}
-        <Button variant="light">Light</Button>{' '}
-        <Button variant="dark">Dark</Button>
-        <Button variant="link">Link</Button>
-      </>
-    );
+
   }
-
-
-        
-
-        
-
-    
-
 
 export default HomePage;
 
